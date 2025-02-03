37feed2dda03a39666d45084afacd55e02f0ebf6
naji611 <najiassi9@gmail.com>
2025-02-03 18:27:23 +0300

    add new api

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const isAuth = require("../middlewares/isAuth.middleware");
 const userController = require("../Controllers/user.controller");
 router.get("/:id", isAuth, userController.userProfile);
-
+router.get("/favorite", isAuth);
 module.exports = router;
