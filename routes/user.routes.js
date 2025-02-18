5bb65bd233e8723cb50ef968b3a7a155459bd3e2
Naji Assi <najiassi9@gmail.com>
2025-02-18 22:12:58 +0300

    testing the webhooks

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,4 +4,10 @@ const isAuth = require("../middlewares/isAuth.middleware");
 const userController = require("../Controllers/user.controller");
 router.get("/:id", isAuth, userController.userProfile);
 router.get("/favorite", isAuth);
+router.get("/wee",(req,res,next)=>{
+    res.send(`<html>
+    <body></body>
+    <h1>Hello from webhook</h1>
+    </html>`)
+})
 module.exports = router;
